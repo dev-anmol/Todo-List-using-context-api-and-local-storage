963b7ae8eb62dc3bc675eec5889d4d5c243f62b3
Anmol Thakur <anmolll.thakurrr@gmail.com>
2023-10-30 19:38:05 +0530

    first commit

---
+++ src/contexts/todoContext.js
@@ -0,0 +1,22 @@
+import { createContext } from "react";
+import { useContext } from "react";
+import React from "react";
+export const TodoContext = React.createContext({
+    todos:[
+        {
+            id:1,
+            todo: "Todo msg",
+            completed: false,
+        }
+    ],
+    addTodo: (todo) => {},
+    updatedTodo: (id, todo) => {},
+    deleteTodo: (id)=> {},
+    toggleComplete: (id) => {}
+})
+
+export const TodoProvider = TodoContext.Provider;
+
+export const useTodo = ()=>{
+    return useContext(TodoContext);
+}
